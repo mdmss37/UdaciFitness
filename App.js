f5bacacb7f658211de36833e0a6f6c965e935e64
kazuya nakajima <uniss010@gmail.com>
2017-11-06 11:40:45 +0900

    slider component set up

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, Text, View, Slider } from 'react-native'
 import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
-  handlePress = () => {
-    alert("hello")
-  }
   render() {
     return (
       <View>
@@ -14,3 +11,13 @@ export default class App extends React.Component {
     );
   }
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginLeft: 10,
+//     marginRight: 10,
+//     alignItems: "stretch",
+//     justifyContent: "center",
+//   }
+// })
