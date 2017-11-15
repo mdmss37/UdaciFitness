ae30bbb7f6e32214e92ca8b4ff7713b40a09e7fe
kazuya nakajima <uniss010@gmail.com>
2017-11-15 16:31:15 +0900

    install redux react-redux

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View, Slider } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry/>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry/>
+        </View>        
+      </Provider>
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     marginLeft: 10,
-//     marginRight: 10,
-//     alignItems: "stretch",
-//     justifyContent: "center",
-//   }
-// })
