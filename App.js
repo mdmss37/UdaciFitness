a88945fffd78b461ca807f09ff8b4019db465b04
kazuya nakajima <uniss010@gmail.com>
2017-11-23 13:20:57 +0900

    add Live

--- App.js
+++ App.js
@@ -10,12 +10,10 @@ import { purple, white, red } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
-
-
+import Live from './components/Live'
 
 const UdaciStatusBar = ({ backgroundColor, ...props}) => (
     <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-      <Text></Text>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
 );
@@ -33,6 +31,13 @@ const Tabs = TabNavigator({
     navigationOptions: {
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
     }
   }
 }, {
